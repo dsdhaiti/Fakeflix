a2c205dffc7feb85e0325639923c2358d14792af
David D'Haiti <sddhaiti@gmail.com>
2019-04-17 10:14:55 -0400

    login/signup page
    user login and signup pages are now ready to recieve code to add  the user to the data base and also check if the user is already in the data base
    
    - data base must be made in mySQL to allow for for it to be able to be hosted on our prospective sitte

--- fake.py
+++ fake.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request,jsonify
 # from wtforms import Form, FloatField, validators
 app = Flask(__name__)
 
@@ -24,21 +24,53 @@ def pricing():
 @app.route("/login")
 
 def login():
-
+    return render_template('regLog.html')
     # form = InputForm(request.form)
     #
     # if request.method == 'POST' and form.validate():
     #     print(request.form['firstname'])
     #     return'<h1> I worked </h1>'
     # else:
-    return render_template('regLog.html')
+
 # print('numbers are:', numbers[ num[0] for num in numbers ])
+@app.route("/forgot_pass")
+def fogot_pass():
+    return render_template('forgot_password.html')
 
-# def loginProcessing():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
+@app.route("/")
 
+@app.route("/loginProcessing",methods=[ 'POST'])
+def loginProcessing():
+    if request.method == 'POST':
+        if request.form['login_form'] == 'Log in':
+
+            username = request.form['username1']
+            password = request.form['password1']
+            print('i worked ')
+
+            output = 'The username is: ' + str(username) + 'the password is: ' + str(password )
+            #    do some processnig with the login information
+            #    check to see if they are in the database
+
+            return jsonify({'output': 'Full Name: ' + output})
+
+        if request.form['login_form'] == 'Sign-Up':
+            firstName = request.form['firstname']
+            lastName =  request.form['lastname']
+            email = request.form['email']
+            password = request.form['confirm_password']
+            output = 'name: ' + firstName + lastName,'emiail: ' + email , ' password: ' + password
+
+            # input processing here sot that a new user will be added to the database
+
+            return jsonify({'output':  output})
+
+
+
+
+
+    # return " <h1> im working i swear</h1>"
+    # return '<h1>' + output + ':' +  'Full Name:' + output + '</h1>'
 
 
 
